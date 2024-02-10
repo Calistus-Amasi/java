bc218c35d11a5bbe8e668ac62c77985366ecfa80
calistus-amasi <amasicalistusemeka@gmail.com>
2024-02-09 16:02:35 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// alert('welcome to Awka');
+// document.write("welcome to Nigeria");
+console.log('today is a great day to code');
+var _peter = 'Welcome to a New Era';
+console.log(_peter);
+var x = 23;
+console.log(x);
+let g = "Nkem";
+console.log(g);
+let AlexJohnson = 50;
+console.log(AlexJohnson);
+const a = "How old is ify =";
+const ify = 27; 
+console.log(a,ify); 
+
+
+let email = "patoski17@gmail.com";
+let first_name = "Patrick";
+const total = first_name + " " +email;
+console.log(total);
+//  full length of the computer
+console.log(total.length);
+
+// to get the uppercase
+console.log(total.toUpperCase());
+let ninjas = total.toLowerCase();
+console.log(ninjas);
+console.log(ninjas,total.toLocaleUpperCase());
+
+
+const title ="one thing is to get to the top another thing is to stay at the top";
+const author = "Patrick";
+const likes = 30;
+
+const solution = `the book called ${title} is written by ${author} is spend to over ${likes} country all over the world`;
+console.log(solution);
+
+// Arrays
